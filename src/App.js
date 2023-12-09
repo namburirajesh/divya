46df9c314e4fcd2b64a8df6a49476d3261c012b3
srnambur <srinivasa.namburi@hexagon.com>
2023-12-09 12:16:43 +0530

    d

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React check this
         </a>
       </header>
     </div>
